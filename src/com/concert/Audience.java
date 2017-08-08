8ebaf2b3f7a98db4d91c0ca370b52b154d80771d
zhaozhipu <zhaozhipu@jd.com>
2017-08-08 09:22:18 +0800

    spring_action初始化

--- src/com/concert/Audience.java
+++ src/com/concert/Audience.java
@@ -1,16 +1,15 @@
 package com.concert;
 
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import com.sun.org.apache.xpath.internal.SourceTree;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class Audience {
 
-    @Before("execution(* *.performance(..))")
+   /* @Before("execution(* *.performance(..))")
     public void silenceCellPhones() {
         System.out.println("Silencing cell phones");
     }
@@ -28,5 +27,40 @@ public class Audience {
     @AfterThrowing("execution(* *.performance(..))")
     public void demandRefund() {
         System.out.println("Demand a refund");
+    }*/
+
+   @Pointcut("execution(* *.performance(..))")
+   public void performance(){}
+
+   /* @Before("performance()")
+    public void silenceCellPhones() {
+        System.out.println("Silencing cell phones");
     }
+
+    @Before("performance()")
+    public void takeSeats() {
+        System.out.println("Taking seats");
+    }
+
+    @AfterReturning("performance()")
+    public void applause() {
+        System.out.println("CLAP CLAP CLAP!!!");
+    }
+
+    @AfterThrowing("performance()")
+    public void demandRefund() {
+        System.out.println("Demand a refund");
+    }*/
+
+   @Around("performance()")
+    public void watchPerformance(ProceedingJoinPoint joinPoint) {
+       try {
+           System.out.println("Silencing cell phones");
+           System.out.println("Taking seats");
+           joinPoint.proceed();
+           System.out.println("CLAP CLAP CLAP CLAP!!!");
+       }catch (Throwable e) {
+           e.printStackTrace();
+       }
+   }
 }
